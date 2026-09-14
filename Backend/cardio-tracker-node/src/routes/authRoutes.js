c8f4364a77b7b1fc985db9727e258b904ede18fb
NguyenVanTrongTri <trongtri14780@gmail.com>
2026-09-14 12:19:06 +0700

    w

--- Backend/cardio-tracker-node/src/routes/authRoutes.js
+++ Backend/cardio-tracker-node/src/routes/authRoutes.js
@@ -4,6 +4,7 @@ const { login } = require('../controllers/authController');
 const { loginLimiter } = require('../middlewares/rateLimiter');
 
 router.post('/login', loginLimiter, login);
+router.post('/register', register);
 
 router.get('/test', (req, res) => {
   res.json({ success: true, message: "Auth route is working!" });

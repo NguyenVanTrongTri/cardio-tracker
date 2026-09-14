791a83534ac0f5b1af64ed378d88ab9398e6dbdf
NguyenVanTrongTri <trongtri14780@gmail.com>
2026-09-14 12:29:27 +0700

    ẻ

--- Backend/cardio-tracker-node/src/routes/authRoutes.js
+++ Backend/cardio-tracker-node/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/authController');
+const { login, register } = require('../controllers/authController');
 const { loginLimiter } = require('../middlewares/rateLimiter');
 
 router.post('/login', loginLimiter, login);

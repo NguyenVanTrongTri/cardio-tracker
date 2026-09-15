b2e00ba3fd61d17c35233b14b4a026317c314575
NguyenVanTrongTri <trongtri14780@gmail.com>
2026-09-15 20:54:36 +0700

    sd

--- Backend/cardio-tracker-node/server.js
+++ Backend/cardio-tracker-node/server.js
@@ -11,6 +11,7 @@ const prisma = new PrismaClient();
 const authRoutes = require('./src/routes/authRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 const workoutRoutes = require('./src/routes/workoutRoutes');
+console.log("Check workoutRoutes:", typeof workoutRoutes);
 const seedRoutes = require('./src/routes/seedRoutes');
 
 // Cấu hình CORS chuẩn
--- Backend/cardio-tracker-node/src/routes/workoutRoutes.js
+++ Backend/cardio-tracker-node/src/routes/workoutRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getWorkouts } = require('../controllers/workoutController');
-
+console.log("Check getWorkouts function:", typeof getWorkouts);
 router.get('/', getWorkouts);
 
 module.exports = router;

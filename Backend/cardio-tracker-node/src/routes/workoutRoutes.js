4b9992f6434f9a0893222ec16994d64dda489fe2
NguyenVanTrongTri <trongtri14780@gmail.com>
2026-09-15 20:48:41 +0700

    ê

--- Backend/cardio-tracker-node/src/routes/workoutRoutes.js
+++ Backend/cardio-tracker-node/src/routes/workoutRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getWorkouts, createWorkout } = require('../controllers/workoutController');
-const authMiddleware = require('../middlewares/authMiddleware'); // Middleware xác thực token đăng nhập
-
-// Áp dụng middleware xác thực cho tất cả các route bên dưới
-router.use(authMiddleware);
+const { getWorkouts } = require('../controllers/workoutController');
 
 router.get('/', getWorkouts);
-router.post('/', createWorkout); // 👉 Thêm dòng này để nhận POST request lưu buổi tập
 
 module.exports = router;

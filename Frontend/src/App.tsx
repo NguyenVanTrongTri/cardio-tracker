f31d96bdf910493d76563be39f33ba6cfaaf1070
NguyenVanTrongTri <trongtri14780@gmail.com>
2026-09-18 13:59:42 +0700

    qư

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -147,7 +147,7 @@ export default function App() {
       return;
     }
     // 👉 2. Xử lý thời gian không hoạt động (Idle Timeout)
-    const IDLE_TIMEOUT_MS = 15 * 60 * 1000; // 15 phút
+    const IDLE_TIMEOUT_MS = 5 * 1000; // 15 phút
     let idleTimer: NodeJS.Timeout;
 
     const handleUserActivity = () => {

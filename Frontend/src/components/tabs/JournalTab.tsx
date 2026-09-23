7b1ac8a1b14e892d68e0e8a0b65c26dd1cc1aaad
NguyenVanTrongTri <trongtri14780@gmail.com>
2026-09-23 14:38:03 +0700

    345thg

--- Frontend/src/components/tabs/JournalTab.tsx
+++ Frontend/src/components/tabs/JournalTab.tsx
@@ -405,6 +405,7 @@ export default function JournalTab() {
                         + Ghi bữa ăn ngay
                       </button>
                     </div>
+                  
                   ) : (
                     journal.meals.map((meal) => (
                       <div
